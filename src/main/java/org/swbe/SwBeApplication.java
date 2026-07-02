76d64beded10e2ca339d275c2662f185170effee
enohs <kmsn01@naver.com>
2026-07-02 18:35:20 +0900

    init: first commit

---
+++ src/main/java/org/swbe/SwBeApplication.java
@@ -0,0 +1,13 @@
+package org.swbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwBeApplication.class, args);
+  }
+
+}
